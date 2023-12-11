b3e20a6e5210575c797709dd3a4c450e74c3c286
chrism-git <cm.info@frontier.com>
2023-12-10 18:57:19 -0800

    commit vslidate

---
+++ ceaserencoder.py
@@ -0,0 +1,60 @@
+# Ceaser cipher program
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+
+
+
+def ceaser(direction, shift, message):
+    text = ""
+    if direction == "decode":
+      shift *= -1
+      shift = shift % 26
+    for char in message:
+      if char in alphabet:
+        position = alphabet.index(char)
+        newposition = position + shift
+        text += alphabet[newposition]
+      else:
+        text += char
+    print(f"The {direction}d message is: \n {text}")
+
+programend = False
+while not programend:
+    direction = input("Do you want to encode or decode the message? \n")
+    shift = int(input(" Enter the shift number: \n"))
+    message = input("Enter the messsage: \n")
+    ceaser(direction,shift,message)
+
+    repeat = input("Do you want to repeat the program? \n").lower()
+    if repeat == "no":
+      print("Program terminated")
+      programend = True
+
+
+
+
+
+

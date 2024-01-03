18a5416f760f091a7792f8573fc24af4c5e5a532
chrism-git <cm.info@frontier.com>
2024-01-03 14:38:15 -0800

    commit update

---
+++ .github/tips.py
@@ -0,0 +1,21 @@
+# Tips Calculator
+
+bill = float(input ("Enter the total amount of the bill: \n"))
+tip = float(input ("Enter the percentage of tip you would like to give: \n"))
+import random
+import math
+
+totaltip = round((tip / 100) * bill,2)
+totalbill = round(totaltip + bill,2)
+
+print(f"The total tip amount is ${totaltip}, and the total bill is ${totalbill}")
+
+decision = input(" Would you like to divide the bill with others? \n").lower()
+
+if decision =="yes":
+  people = int(input("How many people are in the group? \n"))
+  billsplit = round(people / totalbill, 2)
+  print(f"Each person should pay ${billsplit} ")
+
+else:
+  print("Have a nice day")
--- game.py
+++
@@ -1,9 +0,0 @@
-ans = input('Would you like to play a game? ')
-if (ans == 'Y' or 'y'):
-        print('these are the games i have:')
-        print(['1 Tic Tac Toe','2 Chess','3 Checkers','4 Yahtzee','Monopoly'])
-        game=input('Please choose a game: ')
-        print('Thanks, but we are currently experiencing and outage')
-else:
-    print('Ahhh your no fun!')
-    

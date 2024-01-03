c850cf632947fa9f6524559edc8b2067f117a554
chrism-git <cm.info@frontier.com>
2024-01-03 15:12:46 -0800

    commit update

--- .github/Bidprogram.py
+++ .github/Bidprogram.py
@@ -12,6 +12,37 @@ logo = '''
                       /_______________\\
 '''
 
-print(logo)
+print(logo +"\n Welcome to the Auction program")
+
+bidend = False
+
+bids : {}
+
+"""
+def findhighbid(bidamount):
+    highbid = 0
+    for bidder in bidamount:
+    if bidamount > highbid:
+      highbid = bidamount
+"""
+
+while not bidend:
+  name = input("Enter your name: \n")
+  bid = float(input("Enter your bid: \n $"))
+  bids[name] = bid
+  other = input("Are there more bidders? \n").lower()
+
+  if other == "yes":
+    clear()
+  elif other == "no":
+    bidend = True
+
+
+print(bids)
+
+
+
+
+
 
 

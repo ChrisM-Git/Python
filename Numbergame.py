989adaba4ef0847ed0c91e7941660a083bc0d12f
chrism-git <cm.info@frontier.com>
2024-01-13 12:59:29 -0800

    commit update 2

--- Numbergame.py
+++ Numbergame.py
@@ -8,33 +8,52 @@ ___  _                           _                                      _       
 
 
 import random
-print(logo)
-print("Welcome to the number guessing game!")
-print("I am thinking of a number between 1 and 50")
-number = random.randint(1, 50)
-
-difficulty = input("Choose a difficulty, e for easy or h for hard:\n").lower()
-endgame = False
-lives = 0
-if difficulty == "e":
-  lives = 10
-  print("you get 10 tries")
-else:
-  lives = 5
-  print("you get 5 tries")
 
 
-while not endgame:
-  guess = int(input("Guess a number between 1 and 50: \n"))
+
+easylives = 10
+hardlives = 5
+
+
+def answercheck(guess,number,lives):
+
   if guess > number:
-    lives -= 1
     print(f"You guessed to high, you have {lives} tries left")
+    return lives - 1
+
   if guess < number:
-    lives -= 1
     print(f"You guessed to low,you have {lives} tries left")
-  if guess == number:
-    endgame = True
+    return lives - 1
+  else:
     print("Wow, you guessed the right number!")
+
+
+def difficulty():
+  option = input("Choose a difficulty, e for easy or h for hard:\n").lower()
+  if option == "e":
+    return easylives
+  else:
+    return hardlives
+
+def numbergame():
+  number = random.randint(1, 50)
+  print(logo)
+  print("Welcome to the number guessing game!")
+  print("I am thinking of a number between 1 and 50")
+  lives = difficulty()
+  print(f"You get {lives} tries!")
+  guess = 55
+  while guess != number:
+    guess = int(input("Guess a number between 1 and 50: \n"))
+    lives = answercheck(guess,number,lives)
+    if lives == 0:
+      print(f"you didnt guess the number, it was {number}")
+      guess = number
+
+
+while input("Do you want to play guess the number game? (y or n)") == "y":
+  numbergame()
+
 
 
 

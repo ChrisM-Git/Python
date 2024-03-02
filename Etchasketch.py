478c0f215cbece296966733516069a4687bc72ad
Chris M <25236623+ChrisM-Git@users.noreply.github.com>
2024-03-02 09:01:20 -0800

    Add files via upload

---
+++ Etchasketch.py
@@ -0,0 +1,17 @@
+from turtle import Turtle,Screen
+
+timmy = Turtle()
+screen = Screen()
+
+
+def movefwd(space):
+  timmy.forward(10)
+
+  def space():
+    screen.listen()
+    screen.onkeypress(key = "space", fun=movefwd)
+    screen.exitonclick()
+
+
+
+
